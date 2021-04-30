ab730bf078edab948eb5f0591a98aa34afa727ba
Dennis Wei <denniseasycard@gmail.com>
2021-04-30 21:29:35 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// 載入相關套件
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.send('老爸的私房錢')
