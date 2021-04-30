68490224a0fc3aecef40d1f902c5180646498b65
Dennis Wei <denniseasycard@gmail.com>
2021-04-30 22:30:31 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
+const Record = require('./models/record')
 
-app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}))
+app.engine('hbs', exphbs(
+  {
+    defaultLayout: 'main',
+    extname: 'hbs',
+    helpers: {
+      eq: function (v1, v2) { return (v1 === v2) }
+    }
+  }
+))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.send('老爸的私房錢')
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
