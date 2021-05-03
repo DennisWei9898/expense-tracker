0116e4376f6294fbb3e24fe40e531bab1398fdce
Dennis Wei <denniseasycard@gmail.com>
2021-05-03 18:05:08 +0800

    fix: create new expense function

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // 載入相關套件
 const express = require('express')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
+const app = express()
 
 const port = 3000
-const Record = require('./models/record')
-const bodyParser = require('body-parser')
 
 const routes = require('./routes')
 require('./config/mongoose')
-const app = express()
 
 app.engine('hbs', exphbs(
   {
@@ -22,6 +22,7 @@ app.engine('hbs', exphbs(
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(routes)
 
 app.listen(port, () => {
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Record = require('../../models/record')
+const categoryList = require('../../models/seeds/categoryList.json').category
 const router = express.Router()
 
 router.get('/new', (req, res) => {
@@ -7,8 +8,37 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/new', (req, res) => {
-  return Record.create(req.body)
-    .then(() => res.redirect('/'))
+  const { name, date, category, amount } = req.body
+  let iconArray = categoryList.filter((item, index, array) => { return item.category === category })
+  let categoryIcon = iconArray[0].categoryIcon
+  return Record.create({
+    name: name,
+    date: date,
+    category: category,
+    amount: amount,
+    categoryIcon: categoryIcon
+  }
+  )
+    .then(res.redirect('/'))
+    .then(console.log(categoryIcon))
+    .catch(error => console.log(error))
+})
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then(record => res.render('edit', record))
+    .catch(error => console.log(error))
+})
+
+router.put('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => {
+      Object.assign(record, req.body)
+      return record.save()
+    })
+    .then(() => res.redirect(`/records/${id}`))
     .catch(error => console.log(error))
 })
 
