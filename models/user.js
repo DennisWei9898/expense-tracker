a43a7d922b13b514a68b9721a9751f0386e8db60
Dennis Wei <denniseasycard@gmail.com>
2021-06-06 12:27:23 +0800

    feat: add user model and register page

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('../config/mongoose')
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+module.exports = mongoose.model('User', userSchema)
